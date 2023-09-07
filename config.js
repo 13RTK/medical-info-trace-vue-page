0e7bfd3d3a4fefbf4dd1b959897356f44e98dd42
alex <907785012@qq.com>
2023-09-07 08:34:24 +0800

    Init public branch, remove sensitive info

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-export const staffRemotePath = "http://110.40.154.138:8080/api/v1/staff";
-export const staffRemoteGetAllPath =
-    "http://110.40.154.138:8080/api/v1/staff-all";
+export const staffRemotePath = "http://SERVER_IP:8080/api/v1/staff";
+export const staffRemoteGetAllPath = "http://SERVER_IP:8080/api/v1/staff-all";
 export const staffRemoteIssuePath =
-    "http://110.40.154.138:8080/api/v1/staff-issue-all";
-export const issueRemotePath = "http://110.40.154.138:8080/api/v1/issue";
-export const issueRemoteGetAllPath =
-    "http://110.40.154.138:8080/api/v1/issue-all";
+    "http://SERVER_IP:8080/api/v1/staff-issue-all";
+export const issueRemotePath = "http://SERVER_IP:8080/api/v1/issue";
+export const issueRemoteGetAllPath = "http://SERVER_IP:8080/api/v1/issue-all";
 export const issueRemoteCompletePath =
-    "http://110.40.154.138:8080/api/v1/issue-complete";
+    "http://SERVER_IP:8080/api/v1/issue-complete";
